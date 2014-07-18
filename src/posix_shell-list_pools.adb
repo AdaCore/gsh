20fe26069b5caff3a43084cd685b75c8a326ed0c
Nicolas Roche <roche@adacore.com>
2014-07-18 09:09:49 +0200

    Add comments and adjust style

--- src/posix_shell-list_pools.adb
+++ src/posix_shell-list_pools.adb
@@ -1,3 +1,25 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  G S H                                   --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2010-2014, AdaCore                   --
+--                                                                          --
+-- GSH is free software;  you can  redistribute it  and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GSH is distributed in the hope that it will be useful, but WITH-  --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GSH is maintained by AdaCore (http://www.adacore.com)                    --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Posix_Shell.Traces; use Posix_Shell.Traces;
 
 package body Posix_Shell.List_Pools is
@@ -6,6 +28,11 @@ package body Posix_Shell.List_Pools is
      (Pool : in out List_Pool;
       Item : Token)
       return Token_List;
+   --  Create a new element with value Item in the pool
+
+   ------------
+   -- Append --
+   ------------
 
    procedure Append
      (Pool : in out List_Pool; Source : in out Token_List; Item : Token)
@@ -22,6 +49,10 @@ package body Posix_Shell.List_Pools is
       end if;
    end Append;
 
+   ------------
+   -- Append --
+   ------------
+
    procedure Append
      (Pool : in out List_Pool; Source : in out Token_List; Item : Token_List)
    is
@@ -34,22 +65,9 @@ package body Posix_Shell.List_Pools is
       end if;
    end Append;
 
-   function Prepend
-     (Pool : in out List_Pool; Source : Token_List; Item : Token)
-     return Token_List
-   is
-      L : constant Token_List := New_Element (Pool, Item);
-
-   begin
-      pragma Debug (Log ("prepend", Image (Item)));
-      if Source /= Null_List then
-         Pool.Table (L).Next := Source;
-         Pool.Table (L).Last := Pool.Table (Source).Last;
-      else
-         Pool.Table (L).Last := L;
-      end if;
-      return L;
-   end Prepend;
+   ----------------
+   -- Deallocate --
+   ----------------
 
    procedure Deallocate (Pool : in out List_Pool; Source : Token_List) is
       Next_Free : constant Token_List := Pool.Table (1).Next;
@@ -58,17 +76,38 @@ package body Posix_Shell.List_Pools is
       Pool.Table (Pool.Table (Source).Last).Next := Next_Free;
    end Deallocate;
 
+   ----------------
+   -- Deallocate --
+   ----------------
+
+   procedure Deallocate (Pool : in out List_Pool) is
+   begin
+      Free (Pool);
+   end Deallocate;
+
+   -----------------
+   -- Get_Element --
+   -----------------
+
    function Get_Element (Pool : List_Pool; Source : Token_List) return Token
    is
    begin
       return Pool.Table (Source).T;
    end Get_Element;
 
+   --------------
+   -- Is_Empty --
+   --------------
+
    function Is_Empty (Pool : List_Pool; Source : Token_List) return Boolean
    is
    begin
       return Source = 0;
    end Is_Empty;
+
+   -----------------
+   -- New_Element --
+   -----------------
 
    function New_Element
      (Pool : in out List_Pool;
@@ -91,10 +130,9 @@ package body Posix_Shell.List_Pools is
       end if;
    end New_Element;
 
-   function Next (Pool : List_Pool; Source : Token_List) return Token_List is
-   begin
-      return Pool.Table (Source).Next;
-   end Next;
+   --------------
+   -- New_Pool --
+   --------------
 
    function New_Pool return List_Pool is
       Result : List_Pool;
@@ -103,4 +141,36 @@ package body Posix_Shell.List_Pools is
       Append (Result, (Null_Token, 0, 0));
       return Result;
    end New_Pool;
+
+   ----------
+   -- Next --
+   ----------
+
+   function Next (Pool : List_Pool; Source : Token_List) return Token_List is
+   begin
+      return Pool.Table (Source).Next;
+   end Next;
+
+   -------------
+   -- Prepend --
+   -------------
+
+   function Prepend
+     (Pool   : in out List_Pool;
+      Source : Token_List;
+      Item   : Token)
+     return Token_List
+   is
+      L : constant Token_List := New_Element (Pool, Item);
+
+   begin
+      pragma Debug (Log ("prepend", Image (Item)));
+      if Source /= Null_List then
+         Pool.Table (L).Next := Source;
+         Pool.Table (L).Last := Pool.Table (Source).Last;
+      else
+         Pool.Table (L).Last := L;
+      end if;
+      return L;
+   end Prepend;
 end Posix_Shell.List_Pools;
--- src/posix_shell-list_pools.ads
+++ src/posix_shell-list_pools.ads
@@ -1,37 +1,75 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  G S H                                   --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2010-2014, AdaCore                   --
+--                                                                          --
+-- GSH is free software;  you can  redistribute it  and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GSH is distributed in the hope that it will be useful, but WITH-  --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GSH is maintained by AdaCore (http://www.adacore.com)                    --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with GNAT.Dynamic_Tables;
 with Posix_Shell.Lexer; use Posix_Shell.Lexer;
 
 package Posix_Shell.List_Pools is
 
    subtype Token_List is Natural;
+   --  Reference to a list in a pool
+
    type List_Pool is private;
-
-   type Node is private;
-
+   --  Pool of of token lists
 
    Null_List : constant Token_List := 0;
+   --  An empty list
 
    procedure Append
-     (Pool : in out List_Pool; Source : in out Token_List; Item : Token);
+     (Pool   : in out List_Pool;
+      Source : in out Token_List;
+      Item   : Token);
+   --  Append a token to a list. Source contains the new list index
 
    procedure Append
-     (Pool : in out List_Pool; Source : in out Token_List; Item : Token_List);
+     (Pool   : in out List_Pool;
+      Source : in out Token_List;
+      Item   : Token_List);
+   --  Append a list to list. Source contains the new list index
 
    function Prepend
      (Pool   : in out List_Pool;
       Source : Token_List;
       Item   : Token)
      return Token_List;
+   --  Prepend a token to a list
 
    procedure Deallocate (Pool : in out List_Pool; Source : Token_List);
+   --  Free a list from the pool
 
    function Next (Pool : List_Pool; Source : Token_List) return Token_List;
+   --  Get next element of a list
 
    function Get_Element (Pool : List_Pool; Source : Token_List) return Token;
+   --  Get token of the first element in the list Source
 
    function Is_Empty (Pool : List_Pool; Source : Token_List) return Boolean;
+   --  Return true if the list is the empty list
 
    function New_Pool return List_Pool;
+   --  Create a new list pool
+
+   procedure Deallocate (Pool : in out List_Pool);
+   --  Deallocate all memory used by a pool
 
 private
 
@@ -42,7 +80,7 @@ private
    end record;
 
    package List_Pools is new GNAT.Dynamic_Tables
-     (Node, Token_List, 1, 4, 100);
+     (Node, Token_List, 1, 32, 100);
    type List_Pool is new List_Pools.Instance;
 
 end Posix_Shell.List_Pools;
