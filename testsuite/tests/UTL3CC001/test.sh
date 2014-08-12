add4054ff73da8e0294f40c3aa75be75ac9f49b9
Valentine Reboul <reboul@adacore.com>
2014-08-12 18:37:25 +0200

    N805-012 [testing] which and command as builtins
    Change-Id: I9a2dcc6607ff3c9d1880a4a844aedd5e59a475eb

---
+++ testsuite/tests/UTL3CC001/test.sh
@@ -0,0 +1,16 @@
+echo "* type coin"
+type coin
+echo "* command -v coin (should not produce output)"
+command -v coin
+echo "* command -V coin"
+command -V coin
+echo "* which coin"
+which coin
+
+echo "* option -q for the 3 tested builtins"
+type -q
+command -q
+which -q
+
+echo "* command with several but no -v"
+command cp ls echo printf
