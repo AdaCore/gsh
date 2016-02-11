13d1d05bf219c16cceebbe6d6481202bc0fe1572
Nicolas Roche <roche@adacore.com>
2016-02-11 17:44:32 +0100

    Move main units to src/mains
    Change-Id: I18a96f63b3dbc38168d3e00c07dc264d5220022a

---
+++ src/mains/strmatch.adb
@@ -0,0 +1,36 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  G S H                                   --
+--                                                                          --
+--                                                                          --
+--                       Copyright (C) 2010-2016, AdaCore                   --
+--                                                                          --
+-- GSH is free software;  you can  redistribute it  and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  GSH is distributed in the hope that it will be useful, but WITH-  --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- GSH is maintained by AdaCore (http://www.adacore.com)                    --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Sh.Re; use Sh.Re;
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO; use Ada.Text_IO;
+
+function Strmatch return Integer is
+
+begin
+   Put_Line ("pattern: " & Argument (1) & ", str: " & Argument (2));
+   if Match (Argument (2), Argument (1)) then
+      return 0;
+   else
+      return 1;
+   end if;
+end Strmatch;
